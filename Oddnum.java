1100b1dfd38f6172f425578678557803bd9dff3b
AdityaMaheshwari627 <aditya27mh@gmail.com>
2024-09-08 21:55:32 +0530

    Add files via upload

---
+++ Oddnum.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Oddnum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner obj=new Scanner(System.in);
+        System.out.print("Enter size:");
+        int n=obj.nextInt();
+        int[] arr=new int[n];
+        System.out.println("Enter elements:");
+        for(int i=0;i<n;i++) {
+        	arr[i]=obj.nextInt();
+        }
+        System.out.print("Odd elements are:{");
+        for(int i=0;i<n;i++) {
+        	if((arr[i])%2!=0) {
+        		System.out.print(arr[i]+",");
+        	}
+        }
+        System.out.print("}");
+	}
+
+}
